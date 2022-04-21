804dc906bad75e01196bf866100bcb805943f02c
Axel Cola <axelcola1@gmail.com>
2022-04-21 17:22:27 -0300

    changing routes in navbar

--- src/components/NavbarNotes.js
+++ src/components/NavbarNotes.js
@@ -7,7 +7,7 @@ const NavbarNotes = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark mb-5">
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/">
+        <Link className="navbar-brand" to="/sticky-notes-on-react">
           Sticky Notes
         </Link>
         <button
@@ -24,7 +24,11 @@ const NavbarNotes = () => {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/">
+              <Link
+                className="nav-link active"
+                aria-current="page"
+                to="/sticky-notes-on-react"
+              >
                 <BiHomeAlt className="icons" />
               </Link>
             </li>
