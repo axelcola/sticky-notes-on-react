8ef3560fd63572ace97f5ed4adaa7925549d9273
Axel Cola <axelcola1@gmail.com>
2022-05-08 18:43:50 -0300

    footer added

--- src/App.js
+++ src/App.js
@@ -3,17 +3,14 @@ import "./App.css";
 import NoteList from "./components/NoteList";
 import { Route } from "wouter";
 import PaperBin from "./Pages/PaperBin";
-
+import Footer from "./components/Footer";
 function App() {
   return (
     <div className="App">
       <Route path="/sticky-notes-on-react/paper-bin" component={PaperBin} />
       <Route path="/sticky-notes-on-react" component={NoteList} />
       <Route path="/" component={NoteList} />
-      <div className="credits">
-        <h6>Made in React ⚛️ By Axel Cola</h6>
-        <h6>Fueled by ☕ and 🧉</h6>
-      </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <div className="footer credits">
+      <h6>Made in React ⚛️ By Axel Cola - Fueled by ☕ and 🧉</h6>
+    </div>
+  );
+}
