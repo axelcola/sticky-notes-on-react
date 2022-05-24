f85f96d84ef4f27c31443e0f7ec31f9158801175
Axel Cola <axelcola1@gmail.com>
2022-05-24 17:45:53 -0300

    404 error page added

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import NoteList from "./components/NoteList";
 import { Route } from "wouter";
 import PaperBin from "./Pages/PaperBin";
 import Footer from "./components/Footer";
+import errorPage from "./Pages/errorPage";
 function App() {
   return (
     <div className="App">
       <Route path="/sticky-notes-on-react/paper-bin" component={PaperBin} />
       <Route path="/sticky-notes-on-react" component={NoteList} />
       <Route path="/" component={NoteList} />
-      <Route path="/:rest*" component={NoteList} />
+      <Route path="/:rest*" component={errorPage} className="error" />
       <Footer />
     </div>
   );
