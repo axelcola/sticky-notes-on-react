0950294973caf3e0ab371ca7dfac14a5b55a33ef
Axel Cola <axelcola1@gmail.com>
2022-05-25 11:13:57 -0300

    aún no funciona bien en github

--- src/App.js
+++ src/App.js
@@ -8,23 +8,16 @@ import errorPage from "./Pages/errorPage";
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       <Switch>
         <Route path="/sticky-notes-on-react/paper-bin" component={PaperBin} />
         <Route path="/sticky-notes-on-react" component={NoteList} />
-        <Route path="/" component={NoteList} />
         <Route
           path="/sticky-notes-on-react/:rest*"
           component={errorPage}
           className="error"
         />
       </Switch>
-=======
-      <Route path="/sticky-notes-on-react/paper-bin" component={PaperBin} />
-      <Route path="/sticky-notes-on-react" component={NoteList} />
-      <Route path="/" component={NoteList} />
 
->>>>>>> master
       <Footer />
     </div>
   );
